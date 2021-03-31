74f5061ca90ccd53de04650233aa3d27e9e8500f
Peterpaul Klein Haneveld <peterpaul@tppp.home>
2021-03-31 10:12:57 +0200

    Add (de-)serialization functions with explicit serializer

--- src/main/kotlin/com/ing/serialization/bfl/Util.kt
+++ src/main/kotlin/com/ing/serialization/bfl/Util.kt
@@ -3,7 +3,9 @@ package com.ing.serialization.bfl
 import com.ing.serialization.bfl.serde.BinaryFixedLengthInputDecoder
 import com.ing.serialization.bfl.serde.BinaryFixedLengthOutputEncoder
 import com.ing.serialization.bfl.serde.element.Layout
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.modules.EmptySerializersModule
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializer
@@ -13,28 +15,72 @@ import java.io.DataInputStream
 import java.io.DataOutputStream
 
 @ExperimentalSerializationApi
-inline fun <reified T : Any> serialize(data: T, serializersModule: SerializersModule = EmptySerializersModule): ByteArray {
-    val output = ByteArrayOutputStream()
-    val stream = DataOutputStream(output)
-    BinaryFixedLengthOutputEncoder(stream, serializersModule).encodeSerializableValue(serializer(), data)
-    return output.toByteArray()
+inline fun <reified T : Any> serialize(
+    data: T,
+    serializersModule: SerializersModule = EmptySerializersModule
+): ByteArray {
+    return serialize(data, serializer(), serializersModule)
 }
 
 @ExperimentalSerializationApi
-inline fun <reified T : Any> serializeX(data: T, serializersModule: SerializersModule = EmptySerializersModule): Pair<ByteArray, Layout> {
-    val output = ByteArrayOutputStream()
-    val stream = DataOutputStream(output)
-    val bfl = BinaryFixedLengthOutputEncoder(stream, serializersModule)
-    bfl.encodeSerializableValue(serializer(), data)
-    return Pair(output.toByteArray(), bfl.layout)
+inline fun <reified T : Any> serializeX(
+    data: T,
+    serializersModule: SerializersModule = EmptySerializersModule
+): Pair<ByteArray, Layout> {
+    return serializeX(data, serializer(), serializersModule)
 }
 
 @ExperimentalSerializationApi
-inline fun <reified T : Any> deserialize(data: ByteArray, serializersModule: SerializersModule = EmptySerializersModule): T {
-    val input = ByteArrayInputStream(data)
-    val stream = DataInputStream(input)
-    val bfl = BinaryFixedLengthInputDecoder(stream, serializersModule)
-    return bfl.decodeSerializableValue(serializer())
+inline fun <reified T : Any> deserialize(
+    data: ByteArray,
+    serializersModule: SerializersModule = EmptySerializersModule
+): T {
+    return deserialize(data, serializer(), serializersModule)
+}
+
+@ExperimentalSerializationApi
+fun <T> serialize(
+    data: T,
+    serializer: SerializationStrategy<T>,
+    serializersModule: SerializersModule = EmptySerializersModule
+): ByteArray {
+    return ByteArrayOutputStream().use { output ->
+        DataOutputStream(output).use { stream ->
+            BinaryFixedLengthOutputEncoder(stream, serializersModule)
+                .encodeSerializableValue(serializer, data)
+        }
+        output.toByteArray()
+    }
+}
+
+@ExperimentalSerializationApi
+fun <T : Any> serializeX(
+    data: T,
+    serializer: SerializationStrategy<T>,
+    serializersModule: SerializersModule = EmptySerializersModule
+): Pair<ByteArray, Layout> {
+    return ByteArrayOutputStream().use { output ->
+        val layout = DataOutputStream(output).use { stream ->
+            val bfl = BinaryFixedLengthOutputEncoder(stream, serializersModule)
+            bfl.encodeSerializableValue(serializer, data)
+            bfl.layout
+        }
+        Pair(output.toByteArray(), layout)
+    }
+}
+
+@ExperimentalSerializationApi
+fun <T : Any> deserialize(
+    data: ByteArray,
+    deserializer: DeserializationStrategy<T>,
+    serializersModule: SerializersModule = EmptySerializersModule
+): T {
+    return ByteArrayInputStream(data).use { input ->
+        DataInputStream(input).use { stream ->
+            val bfl = BinaryFixedLengthInputDecoder(stream, serializersModule)
+            bfl.decodeSerializableValue(deserializer)
+        }
+    }
 }
 
 fun <T> ArrayDeque<T>.prepend(value: T) {

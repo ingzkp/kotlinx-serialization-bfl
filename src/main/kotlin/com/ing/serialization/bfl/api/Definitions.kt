e3c82e672ebabc86e966833b137446eab49b416d
Victor Ermolaev <victorermolaev@gmail.com>
2021-04-01 17:14:45 +0200

    Add base definitions for easy serializer definitions.

---
+++ src/main/kotlin/com/ing/serialization/bfl/api/Definitions.kt
@@ -0,0 +1,27 @@
+package com.ing.serialization.bfl.api
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+open class BaseSerializer<T, S : Surrogate<T>>(
+    private val strategy: KSerializer<S>,
+    private val make: (T) -> S
+) : KSerializer<T> {
+    override val descriptor: SerialDescriptor = strategy.descriptor
+
+    override fun deserialize(decoder: Decoder): T {
+        val surrogate = decoder.decodeSerializableValue(strategy)
+        return surrogate.toOriginal()
+    }
+
+    override fun serialize(encoder: Encoder, value: T) {
+        val surrogate = make(value)
+        encoder.encodeSerializableValue(strategy, surrogate)
+    }
+}
+
+interface Surrogate<T> {
+    fun toOriginal(): T
+}

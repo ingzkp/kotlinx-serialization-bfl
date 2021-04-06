1f3e23c2ce8812e3f521cadce9c66f2b2f5f9463
SereV94 <vasileios.serentellos@ing.com>
2021-04-06 16:54:59 +0200

    Add test for unknown serializer

--- src/test/kotlin/com/ing/serialization/bfl/serde/exceptions/MissingSerializersTest.kt
+++ src/test/kotlin/com/ing/serialization/bfl/serde/exceptions/MissingSerializersTest.kt
@@ -2,6 +2,7 @@ package com.ing.serialization.bfl.serde.exceptions
 
 import com.ing.serialization.bfl.api.serialize
 import com.ing.serialization.bfl.serde.SerdeError
+import io.kotest.matchers.string.shouldContain
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Polymorphic
 import kotlinx.serialization.Serializable
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class MissingSerializersTest {
+    enum class STATUS { ON }
+
     @Test
     fun `serialization with missing Contextual serializer should fail`() {
         @Serializable
@@ -26,6 +29,18 @@ class MissingSerializersTest {
 
         assertThrows<SerdeError.NoPolymorphicSerializers> {
             serialize(Data(1))
+        }
+    }
+
+    @Test
+    fun `serialization with unknown serializer should fail`() {
+        @Serializable
+        data class Data(val value: STATUS)
+
+        assertThrows<IllegalStateException> {
+            serialize(Data(STATUS.ON))
+        }.also {
+            it.message shouldContain "Do not know how to build element from type"
         }
     }
 }
